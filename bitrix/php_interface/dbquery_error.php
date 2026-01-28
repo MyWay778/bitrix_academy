798a1dbf1b5dcee963fb8f9ed8e1a2a5e2452cdc
MyWay778 <sakla4onakne@mail.ru>
2026-01-28 02:33:54 +0000

    init

---
+++ bitrix/php_interface/dbquery_error.php
@@ -0,0 +1,8 @@
+<!-- /bitrix/php_interface/dbquery_error.php -->
+<html>
+<head><title>Технические работы</title></head>
+<body>
+    <h1>Извините, сайт временно недоступен</h1>
+    <p>Мы уже работаем над исправлением ошибки в базе данных.</p>
+</body>
+</html>
